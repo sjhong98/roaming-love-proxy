5d8ae6164a16139131ed860b83d1191c4e3ddb39
seungjaehongibrdos <162073073+seungjaehongibrdos@users.noreply.github.com>
2025-11-20 03:04:20 +0900

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ export default function Home() {
     const deepLink = 'exp+://expo-development-client/?url=https://u.expo.dev/4912cd96-d87f-41b4-b634-6f1e04cc271e/group/8f421043-89a4-4d6e-a23b-05d07cc36504'
     window.location.href = deepLink
   }, [])
+  
 
   return (
     <></>
